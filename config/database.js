89fb5800ffcdeb3bd568932a42bb579b0c8a4ad1
caiquegl <arq.caique@hotmail.com>
2020-06-16 13:21:47 -0300

    testando

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-  username: "b6b301a96b07e5",
-  password: "ddc1b873",
-  database: "heroku_aea1eeb9296486e",
-  host: "us-cdbr-east-05.cleardb.net",
+  username: "e6tuthesisox9xln",
+  password: "p44bqak2v7efuviv",
+  database: "yxpgjxkfw5w7beds",
+  host: "r4919aobtbi97j46.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
   port: 3306,
  
